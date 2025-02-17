228e2ac8652af1e7b374a18ac40b5f3c9bba022b
Samuel Dayne <samuel.dayne2008@gmail.com>
2025-02-17 09:19:04 -0300

    Aula sobre estrutura e códigos básicos em java

---
+++ aula01/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Indiano + boliviano + japones + lesbica + molestador + Macaco Narigudo + cafetão + Twitter Girl + Eminem + Emo = Brinquedão");
+
+  }
+}
---
+++ aula01/Numero.java
@@ -0,0 +1,9 @@
+public class Numero {
+    public static void main(String[] args) {
+        System.out.println(9 + 1);
+        System.out.println(9 - 1);
+        System.out.println(9 * 2);
+        System.out.println(9 / 3);
+}
+
+}
