9f393592bfc839c6f24f472b805d9145fb77e2c1
Samuel Lirio Dayne <samuel.dayne2008@gmail.com>
2025-02-24 09:30:58 -0300

    aula de java

---
+++ aula02/Variaveis.java
@@ -0,0 +1,16 @@
+public class Variaveis {
+    public static void main(String[] args) {
+      String msg = "Ola tudo bom?";
+      System.out.println(msg);
+      int n1 = 3;
+      int n2 = 4;
+      int n4 = 5;
+      int n5 = 6;
+      int n6 = 7;
+      System.out.println(n5 + n4);
+      System.out.println(n6 - n2);
+      System.out.println(n6 / n1);
+      System.out.println(n7 + n2);      
+    }
+  }
+  
