d0759d446b8ae84384e0e408ca817cc30f0fc30a
Samuel Dayne <samuel.dayne2008@gmail.com>
2025-04-28 09:19:33 -0300

    Aula 05 java netbeans

---
+++ Aula005/src/aula005/Exercicio01.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package aula005;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class Exercicio01 {
+    public static void main(String[] args)        
+    {
+               
+        double GastoJaneiro = 15000;
+        double GastoFevereiro = 23000;
+        double GastoMarco = 17000;
+        double GastoTrimestral = GastoJaneiro + GastoFevereiro + GastoMarco;
+        
+        System.out.println("Seu gasto trimestral é: " + GastoTrimestral);
+    }
+    
+}
